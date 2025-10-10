4f33fa21f7cfb669fd0d2ce5c9843687e3421c13
Henrym718 <henrym.718@gmail.com>
2025-10-09 22:26:11 -0500

    resolve Husky pre-commit issues and lint-staged setup

--- src/domain/entities/Account .ts
+++ src/domain/entities/Account .ts
@@ -1,4 +1,3 @@
-
 import { Id } from '../value-objects/Id';
 import { ProviderId } from '../value-objects/ProviderId';
 import { Password } from '../value-objects/Password';
@@ -6,44 +5,43 @@ import { CreatedAt } from '../value-objects/CreatedAt';
 import { UpdatedAt } from '../value-objects/UpdatedAt ';
 
 export interface AccountProps {
-
-    id: Id;
-    userId: Id;
-    accountId: Id;
-    providerId: ProviderId;
-    password?: Password;
-    createdAt: CreatedAt;
-    updatedAt: UpdatedAt;
+  id: Id;
+  userId: Id;
+  accountId: Id;
+  providerId: ProviderId;
+  password?: Password;
+  createdAt: CreatedAt;
+  updatedAt: UpdatedAt;
 }
 
 export class Account {
-    constructor(private readonly props: AccountProps) { }
+  constructor(private readonly props: AccountProps) {}
 
-    get id(): Id {
-        return this.props.id;
-    }
+  get id(): Id {
+    return this.props.id;
+  }
 
-    get userId(): Id {
-        return this.props.userId;
-    }
+  get userId(): Id {
+    return this.props.userId;
+  }
 
-    get accountId(): Id {
-        return this.props.accountId;
-    }
+  get accountId(): Id {
+    return this.props.accountId;
+  }
 
-    get providerId(): ProviderId {
-        return this.props.providerId;
-    }
+  get providerId(): ProviderId {
+    return this.props.providerId;
+  }
 
-    get password(): Password | undefined {
-        return this.props.password;
-    }
+  get password(): Password | undefined {
+    return this.props.password;
+  }
 
-    get createdAt(): CreatedAt {
-        return this.props.createdAt;
-    }
+  get createdAt(): CreatedAt {
+    return this.props.createdAt;
+  }
 
-    get updatedAt(): UpdatedAt {
-        return this.props.updatedAt;
-    }
+  get updatedAt(): UpdatedAt {
+    return this.props.updatedAt;
+  }
 }
